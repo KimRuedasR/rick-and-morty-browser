4f3948fbd3c7220ebe3210b28e981ed1ade19914
KimRuedasR <kruedasr@gmail.com>
2025-11-07 15:59:17 -0600

    feat: Define graphql types and interfaces, add character list

---
+++ src/graphql/queries.ts
@@ -0,0 +1,21 @@
+import { gql } from "@apollo/client";
+
+export const GET_CHARACTERS = gql`
+  query GetCharacters($page: Int, $name: String) {
+    characters(page: $page, filter: { name: $name }) {
+      info {
+        count
+        pages
+        next
+        prev
+      }
+      results {
+        id
+        name
+        status
+        species
+        image
+      }
+    }
+  }
+`;
---
+++ src/types/graphql.ts
@@ -0,0 +1,20 @@
+import type { Character } from "./character";
+
+export interface PageInfo {
+  count: number;
+  pages: number;
+  next: number | null;
+  prev: number | null;
+}
+
+export interface GetCharactersQueryResponse {
+  characters: {
+    info: PageInfo;
+    results: Character[];
+  };
+}
+
+export interface GetCharactersQueryVariables {
+  page?: number;
+  name?: string;
+}
